1c52133e5f7cc8d4172eedb5ed721d980098c180
Hanmin <rlemdhs2747@naver.com>
2024-09-12 18:06:00 +0900

    hw1

---
+++ hw1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main(void) {
+
+	//개념 확인 과제
+	int num1, num2;
+	printf("Input two integers: ");
+	scanf("%d", &num1);
+	scanf("%d", &num2);
+	printf("%d & %d = %d\n", num1, num2, num1 & num2);
+	printf("%d | %d = %d\n", num1, num2, num1 | num2);
+	printf("%d ^ %d = %d\n", num1, num2, num1 ^ num2);
+
+
+
+
+
+
+}
